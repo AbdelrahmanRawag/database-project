9f3ee61882dd2dd5781deb7f89d94c44dfd33315
AbdelrahmanRawag <32689375+AbdelrahmanRawag@users.noreply.github.com>
2017-12-08 21:18:05 +0200

    DATABASE3

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# 
 # A buggy web service in need of a database.
 import psycopg2
 from flask import Flask, request, redirect, url_for
@@ -47,23 +46,24 @@ br = "<br>"
 def main():
     db = psycopg2.connect(database="news")
     c = db.cursor()
-    c.execute("select * from m_articles ;")
+    c.execute("SELECT * FROM m_articles ORDER BY koko DESC LIMIT 3;")
     art = c.fetchall()
 
     answers = "".join(POST % (title, koko) for title, koko in art)
     answers += br
     answers += br
     answers += br
-    c.execute("select * from m_authors ;")
+    c.execute("SELECT * FROM m_authors ORDER BY koko DESC ;")
     aut = c.fetchall()
     answers += "".join(POST % (name, koko) for name, koko in aut)
     answers += br
     answers += br
     answers += br
 
-    c.execute \
-        ("select date , cast(errors as decimal)/cast(total_requests as decimal) *"
-         " 100 as per from dates_errors  order by per desc ;")
+    c.execute(
+        "SELECT date , cast(errors as decimal)"
+        "/cast(total_requests as decimal) *"
+        " 100 as per FROM dates_errors  ORDER BY per DESC LIMIT 1 ;")
     er = c.fetchall()
     answers += "".join(POST % (date, per) for date, per in er)
     db.close()
